13ccc204eb262470b13d78136a712df0a14bc10a
Ashurova Irina <IAshurova@SRF.local>
2022-11-05 17:22:46 +0700

    files added

---
+++ 2/index.js
@@ -0,0 +1,23 @@
+function sumUp() {
+    let x = +prompt('Введите первое число')
+    let y = +prompt('введите второе число')
+    alert(x + y)
+}
+
+function subtract() {
+    let x = prompt('Введите первое число')
+    let y = prompt('введите второе число')
+    alert(x - y)
+}
+
+function devide() {
+    let x = prompt('Введите первое число')
+    let y = prompt('введите второе число')
+    alert(x / y)
+}
+
+function multiply() {
+    let x = prompt('Введите первое число')
+    let y = prompt('введите второе число')
+    alert(x * y)
+}
